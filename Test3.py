2197b7f4c0195e6d3a1cc69f6e0214e7c948e3a5
B5DX <wrwzx1@163.com>
2020-03-06 09:59:05 +0800

    day test

--- Test3.py
+++ Test3.py
@@ -22,10 +22,30 @@ def chList(L: list) :
     print(L)
     print("L重新赋值后的地址:",id(L))
 
+def lambdaTest(): #熟悉filter, map函数用法, 注意res需要list()一下才可直接打印
+    L = [i for i in range(1, 21)]
+    res = filter(lambda x: x%2!=0, L)
+    print(list(res))
+
+def test(x, *args):#学习join的用法（前面的str插孔加入到后面迭代器的序列)
+    print("Hi,", ', '.join(args))
+
+def argsTest(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
 if __name__ == "__main__":
     # w, p = map(float, input('Enter the weight and price:').split())
     # print(f'Pay {buy(w, p)}')
-    L = [1,2]
-    print(id(L))
-    chList(L)
-    print(id(L))
+    
+    # L = [1,2]
+    # print(id(L))
+    # chList(L)
+    # print(id(L))
+
+    # lambdaTest()
+    # args = ('a'+'b')
+    # print('a'+'b')
+    # print(args)
+    # print(*args)
+    argsTest(1,2,3,a=1,b=2,c=3)
